e72c448295329529e46ec611656be3626d783e5a
Niklas Birth <birth@posteo.de>
2023-08-22 10:39:52 +0200

    Trying to get the OSX ARM build to work

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
+autoreconf -i
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
-autoreconf -i
 ./configure --prefix=$PREFIX --enable-mmap	
 make
 make install
